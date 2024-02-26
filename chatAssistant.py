929a86debe43a4889b8969ac6e006e0e8f8dbde9
Vinicius Tom Vidal <viniciustom@hotmail.com>
2024-02-26 17:17:53 -0300

    Background

--- chatAssistant.py
+++ chatAssistant.py
@@ -172,9 +172,9 @@ if st.session_state.start_chat:
             st.markdown(message["content"])
 
 
-
+    prompt = st.chat_input("Faça uma pergunta!") | pergunta_
     # Campo pro usuário escrever
-    if prompt := st.chat_input("Faça uma pergunta!") | pergunta_:
+    if prompt | pergunta_:
         # Adiciona as mensagens do usuário e mostra no chat
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
