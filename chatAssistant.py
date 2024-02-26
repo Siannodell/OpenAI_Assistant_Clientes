41d084e46c863db96f7fbe730168318d2060535e
Vinicius Tom Vidal <viniciustom@hotmail.com>
2024-02-26 16:16:37 -0300

    Background

--- chatAssistant.py
+++ chatAssistant.py
@@ -107,6 +107,7 @@ on = st.sidebar.toggle('Ver sugestões de perguntas')
 
 if on:
     st.sidebar.write('Feature activated!')
+    st.sidebar.write('<style>.st-bx {background: #282828}</style>')
 
 # Define a função para iniciar
 def process_message_with_citations(message):
