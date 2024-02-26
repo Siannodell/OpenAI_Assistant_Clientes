883439cbf03c215e443c05cb11d5f276380d9c72
Vinicius Tom Vidal <viniciustom@hotmail.com>
2024-02-26 16:35:56 -0300

    Background

--- chatAssistant.py
+++ chatAssistant.py
@@ -62,7 +62,7 @@ if api_key:
 
 # Execute your app
 icon_copy = """
-<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="white" height="80px" width="80px" version="1.1" viewBox="0 0 512 512" enable-background="new 0 0 512 512">
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="white" height="16px" width="16px" version="1.1" viewBox="0 0 512 512" enable-background="new 0 0 512 512">
   <g>
     <g>
       <path d="M480.6,109.1h-87.5V31.4c0-11.3-9.1-20.4-20.4-20.4H31.4C20.1,11,11,20.1,11,31.4v351c0,11.3,9.1,20.4,20.4,20.4h87.5    v77.7c0,11.3,9.1,20.4,20.4,20.4h341.3c11.3,0,20.4-9.1,20.4-20.4v-351C501,118.3,491.9,109.1,480.6,109.1z M51.8,362V51.8h300.4    v57.3H139.3c-11.3,0-20.4,9.1-20.4,20.4V362H51.8z M460.2,460.2H159.7V150h300.4V460.2z"/>
