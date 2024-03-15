0bae6eedaa458669cc60fd812e796549ce34870a
Vinicius Tom Vidal <viniciustom@hotmail.com>
2024-03-15 17:30:39 -0300

    Botão

--- chatAssistant.py
+++ chatAssistant.py
@@ -99,7 +99,7 @@ if api_key:
 #st.sidebar.write("<a style='color:white'  href='https://tecnologia2.chleba.net/_ftp/chatgpt/BotasVentoPedidos.xlsx' id='baixarArquivo'>[Baixe o arquivo para fazer a análise]</a>", unsafe_allow_html=True)
 
 #uploaded_file = st.sidebar.file_uploader("Envie um arquivo", key="file_uploader")
-uploaded_file = download_file("https://tecnologia2.chleba.net/_ftp/chatgpt/BotasVentoPedidos.xlsx")
+#uploaded_file = download_file("https://tecnologia2.chleba.net/_ftp/chatgpt/BotasVentoPedidos.xlsx")
 # Botão para iniciar o chat
 
 if not st.session_state.start_chat:
